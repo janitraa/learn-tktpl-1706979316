5af63dc2563f8aa1d3703ec956eb574130343aed
Janitra Ariena <janitra.a.s@gmail.com>
2020-10-05 20:52:12 +0700

    git ignore

--- tktpl-janitra/app/src/main/java/id/ac/ui/cs/mobileprogramming/janitra/tktpl/data/Motivation.kt
+++
@@ -1,9 +0,0 @@
-package id.ac.ui.cs.mobileprogramming.janitra.tktpl.data
-
-data class Motivation(val motivationText: String,
-                 val name: String) {
-
-    override fun toString(): String {
-        return "$name - $motivationText"
-    }
-}
